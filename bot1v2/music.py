fcb45ead80a9510704d0b2aaf9dec496a9444b2f
rjm07270 <69855335+rjm07270@users.noreply.github.com>
2021-12-20 22:01:23 -0500

    --Fust upload of music bot code for private use ONLY

--- bot1v2/music.py
+++ bot1v2/music.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import youtube_dl
 import asyncio
+from threading import Thread
 
 
 class NoTracksFound(commands.CommandError):
@@ -11,17 +12,21 @@ class music(commands.Cog):
     def __init__(self, client):
         self.client =client
         self.queues={}
-        self.Tqueues={}
-        self.players={}
-        self.is_playing=False
-
+        self.ctxs={}
         
-    def check_queue(self,ctx,id):
+        
 
+
+    def queue_start(self,ctx,id):
+        
         if self.queues[id] != []:
             source =self.queues[id].pop(0)
+            print(ctx)
             voice =ctx.guild.voice_client
-            player=voice.play(source, after=lambda x=None: self.check_queue(ctx,id))
+            player=voice.play(source, after=lambda x=None: self.queue_start(ctx,id))
+        else:
+            del self.queues[id]
+
 
 
     @commands.command()
@@ -51,11 +56,11 @@ class music(commands.Cog):
                 await voice_channel.connect()
 
 
-        ctx.voice_client.stop()
+        
         url=url.strip("<>")
         FFMPEG_OPTIONS = {'before_options':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
         YDL_OPTIONS = {'format': "bestaudio"}
-        vc = ctx.voice_client
+        
         
         info=None
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
@@ -65,43 +70,28 @@ class music(commands.Cog):
             source = await discord.FFmpegOpusAudio.from_probe(ur12, **FFMPEG_OPTIONS)
             
         guild_id = ctx.message.guild.id
-
-
-        vc.play(source, after=lambda x=None: self.check_queue(ctx,guild_id))
+        voice =ctx.guild.voice_client
+        if guild_id in self.queues:
+            self.queues[guild_id].append(source)
+            await ctx.send("Song added")
+        else:
+            
+            self.queues[guild_id] = [source]
+            self.queue_start(ctx, guild_id)
+            await ctx.send("Starting voice chat")
+                
+            
+            
         
-        
-
     @commands.command()
     async def pause(self,ctx):
-        await ctx.voice_client.pause()
+        ctx.voice_client.pause()
         await ctx.send("pause")
 
     @commands.command()
     async def stop(self,ctx):
-        await ctx.voice_client.stop()
+        ctx.voice_client.stop()
         await ctx.send("stopped")
 
-    @commands.command()
-    async def queue(self,ctx,url):
-        server= ctx.guild.voice_client
-        FFMPEG_OPTIONS = {'before_options':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn'}
-        YDL_OPTIONS = {'format': "bestaudio"}
-        info=None
-        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-            info = ydl.extract_info(url, download=False)
-            ur12 =info['formats'][0]['url']
-            
-            source = await discord.FFmpegOpusAudio.from_probe(ur12, **FFMPEG_OPTIONS)
-            
-        guild_id = ctx.message.guild.id
-
-        if guild_id in self.queues:
-            self.queues[guild_id].append(source)
-            self.Tqueues[guild_id].append(info['duration'])
-        else:
-            self.queues[guild_id]= [source]
-            self.Tqueues[guild_id]= [info['duration']]
-        
-        await ctx.send("added to queue")
 def setup(client):
     client.add_cog(music(client))
